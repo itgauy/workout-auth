186760596f67be399838776fe58599cdd1e4df4a
itgauy <torreslian@duck.com>
2024-06-05 15:06:31 +0800

    User Routes, Controller & Model

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
@@ -10,20 +11,21 @@ const app = express()
 // middleware
 app.use(express.json())
 
-app.use((_req, _res, _next) => {
-  console.log(_req.path, _req.method)
-  _next()
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
 
-//routes
+// routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected on db and listening on port', process.env.PORT)
+      console.log('connected to db & listening on port', process.env.PORT)
     })
   })
   .catch((error) => {
